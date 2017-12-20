822048c5b89fb4b3731865aecfd64bd30b8735fd
Colin <colinkinzel@gmail.com>
2017-12-20 12:29:06 -0600

    adding /MurderRunGame forcestart

--- MurderRunCommands.java
+++ MurderRunCommands.java
@@ -4,9 +4,16 @@ public class MurderRunCommands() implement CommandExecutor {
   public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     Player p = (Player) sender;
     if(args[0] == "start") {
-    
+      if(pamount < main.getConfig().getInt("Misc.PlayersMin") {
+        p.sendMessage(ChatColor.RED + "Error: Not enough players!");
+        main.gameStatus("DISABLED");
+      }
     } else if(args[0] == "forcestart"){
-    
+        if(pamount < main.getconfig().getInt("Misc.PlayersMin") {
+          p.sendMessage(ChatColor.ORANGE + ChatColor.BOLD + "Warning" + ChatColor.RESET + ChatColor.ORANGE + "Not many players! "); 
+        } else if(pamount < 2) {
+           p.sendMessage(ChatColor.RED + "Error: Can't force start as their is " + main.getConfig().getInt("Misc.PlayersMin") + " players required");
+        }
     } else if(args[0] == "stop") {
     
     } else if(args[0] == "reload") {
